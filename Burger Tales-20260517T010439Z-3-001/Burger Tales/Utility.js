d21661f284abba950ea062dcb6cdcf1bb050d429
codeInvaild <kongtrong21@gmail.com>
2026-08-04 00:42:26 -0600

    added shake and randomInt functions
    you can now create a random integer using a glorified math.random() function and also easily create shake effects on objects that have a present x and y value to modify directly

--- Burger Tales-20260517T010439Z-3-001/Burger Tales/Utility.js
+++ Burger Tales-20260517T010439Z-3-001/Burger Tales/Utility.js
@@ -1,9 +1,17 @@
+import {battlefield} from "./BattleHandler.js";
+
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 export let renderedShapes = {}
 
 export const LERP = (a,b,t) => a + (b-a) * t
+
+export function randInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export function normalizeVector(vector) {
     const length = Math.sqrt(vector.x * vector.x + vector.y * vector.y);
@@ -49,12 +57,51 @@ export function newText(id,x,y,color="rgb(255,255,255)",font = "16px Arial",text
     renderedShapes[id] = {X:x,Y:y,Color:color,Font : font, Text : text, draw : function(){
             ctx.font = renderedShapes[id].Font;
             ctx.lineWidth = 8;
-            ctx.strokeStyle = "black";
-            ctx.strokeText(renderedShapes[id].Text, x, y);
+            // ctx.strokeStyle = "black";
+            // ctx.strokeText(renderedShapes[id].Text, x, y);
             ctx.fillStyle = renderedShapes[id].Color;
             ctx.fillText(renderedShapes[id].Text, renderedShapes[id].X, renderedShapes[id].Y);
         }}
     return renderedShapes[id];
 }
 
+export class shakeEffect {
+    static shakes = []
+    static update = function(deltaTime){
+        for (let shakeIndex =0;shakeIndex < shakeEffect.shakes.length; shakeIndex++) {
+            let curShake = shakeEffect.shakes[shakeIndex];
+            curShake.elapsedTime += deltaTime;
+
+            let proportionalElapsed = curShake.elapsedTime / curShake.duration;
+            let actualShakeMag = curShake.decay ? (1 - proportionalElapsed) * curShake.magnitude: curShake.magnitude
+
+            curShake.object.x = curShake.data.originalX + randInt(-actualShakeMag, actualShakeMag);
+            curShake.object.y = curShake.data.originalY + randInt(-actualShakeMag, actualShakeMag);
+            if (curShake.elapsedTime >= curShake.duration) {
+                curShake.object.x = curShake.data.originalX;
+                curShake.object.y = curShake.data.originalY;
+                shakeEffect.shakes.splice(shakeIndex,1);
+                break;
+            }
+        }
+    };
+
+    constructor(object,magnitude,duration, decay = true) {
+        this.data = {
+            originalX:object?.x,
+            originalY:object?.y,
+        }
+        this.decay = decay;
+        this.object = object;
+        this.magnitude = magnitude;
+        this.elapsedTime = 0;
+        this.duration = duration;
+        shakeEffect.shakes.push(this);
+    }
+}
+
+
+
 export function drawAllShapes() {for (const shape in renderedShapes) {renderedShapes[shape].draw()}}
+
+export let scenePool = {};
