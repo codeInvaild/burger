17423ff693101f779cd345fbeae207e43f5b4192
codeInvaild <kongtrong21@gmail.com>
2026-08-04 00:40:29 -0600

    added player action functionality
    Created a blueprint as to how the player actions should operate, minigames function successfully!

--- Burger Tales-20260517T010439Z-3-001/Burger Tales/actionDirectory.js
+++ Burger Tales-20260517T010439Z-3-001/Burger Tales/actionDirectory.js
@@ -1,7 +1,8 @@
 import { tweenService } from "./TweenService.js";
-import  { keys, keybinds } from "./KeyboardInputHandler.js";
-import { battlefield } from "./BattleHandler.js";
-import { newText, newRect } from "./Utility.js";
+import {keys, keybinds, keyPresses, keyReleases} from "./KeyboardInputHandler.js";
+import {allyXBuffer, battle, battlefield, battleStates, heightAddon, setState} from "./BattleHandler.js";
+import { newText, newRect, randInt,shakeEffect } from "./Utility.js";
+import {gameHeight,gameWidth} from "./main.js";
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
@@ -146,23 +147,52 @@ export let actionDirectory = {//THESE ARE USED FOR DODGING
             timeline.at(0.5,() => {
                 tweenService.create(battlefield[targetSide][target].worldData, tweenService.TweenInfo(1,"SineOut"),{x:400}).play();
             });
-        }
+        },
+        targetType : "enemies",
     }
 }
 
 export let playerActionDirectory = {
-    sword : {
+    slash : {
         cost : 5,
-        execute : function(target,targetSide){
-            let timeline = AnimationManager.create(6);
-            timeline.at(0.5,() => {
+        execute : function(self,target,targetSide){
+            console.log("successfully triggered")
+            console.log(self);
+            tweenService.create(self.worldData,tweenService.TweenInfo(1,"SineOut"),{x:target.worldData.x - (self.worldData.width + 20)}).play();
+            setTimeout(function(){
+                let c = new attackMinigames("sword",{},self,target);
+            },1000)
                 //tween ally toward the enemy (stop right in from of them)
                 //create a sword png, have the player holster it on their back, and swing in an arc
-            })
-        }
-    },
-    staff : {
+                //create a hold and release mechanism and determine pt bonus based on success (closer to the center of the green hit zone, the >40 pts you get)
 
+                //create two bars; a brown and a green zone, the green zone is contained within the brown zone and size is determined by a percent modifier
+                //create a red tick that sticks out on the top and bottom that the player can hold their interaction button to hold and reelase from
+
+                //the timeline system sadly can't really work here and I also need some way for this to get input whilst animate
+        },
+        targetType : "enemies",
+    },
+    wack : {
+        cost : 5,
+        execute : function(self,target,targetSide){
+            console.log("successfully triggered")
+            tweenService.create(self.worldData,tweenService.TweenInfo(0.5,"ElasticOut"),{x:self.worldData.x + 100}).play();
+            setTimeout(function(){
+                let c = new attackMinigames("spell",{},self,target);
+            },250)
+            //tween ally toward the enemy (stop right in from of them)
+            //create a sword png, have the player holster it on their back, and swing in an arc
+            //create a hold and release mechanism and determine pt bonus based on success (closer to the center of the green hit zone, the >40 pts you get)
+
+            //create two bars; a brown and a green zone, the green zone is contained within the brown zone and size is determined by a percent modifier
+            //create a red tick that sticks out on the top and bottom that the player can hold their interaction button to hold and reelase from
+
+            //the timeline system sadly can't really work here and I also need some way for this to get input whilst animate
+
+
+        },
+        targetType : "enemies",
     },
     hammer : {
 
@@ -172,10 +202,100 @@ export let playerActionDirectory = {
     }
 }
 
+export class attackMinigames {
+    static existingMinigames = [];
+    static updateCurrentMinigames(deltaTime) {attackMinigames.existingMinigames[0]?.update(deltaTime);}; //ONLY ONE SHOULD BE ACTIVE AT A TIME
+
+    constructor(name, modifiers,self,target){
+        this.update = null;
+        switch (name) {
+            case "sword":
+                this.objects = {
+                    tickWidth : 10,
+                    tickHeight:100,
+                    tickX : 820,
+                    tickY : 800,
+                    tickColor : "rgb(255,0,0)",
+                    tickSpeed: modifiers?.tickSpeed ?? 12,
+
+                    boxWidth : 600,
+                    boxHeight : 100,
+                    boxX: (gameWidth/2) - 300,
+                    boxY : 800,
+                    boxColor : "rgb(37,77,125)",
+
+                    boxOutlineSize : 5,
+
+                    padAreaWidth: modifiers?.padAreaWidth ?? 70,
+                    padAreaHeight:100,
+                    padAreaX: modifiers?.padAreaX ?? randInt(800 + (modifiers?.padAreaWidth ?? 40),400 + 800 - (modifiers?.padAreaWidth ?? 40)),
+                    padAreaY : 800,
+                    padColor : "rgb(62,143,57,0.8)",
+
+                    reverse:false,
+                }
+                this.update = function(deltaTime){//do not use update to draw things, it WILL NOT show up!
+
+                    if (this.objects.tickX >= (this.objects.boxX + this.objects.boxWidth) - this.objects.tickWidth || this.objects.tickX < this.objects.boxX + this.objects.tickWidth) {
+                        this.objects.reverse = !this.objects.reverse;
+                    }
+                    //edge case catching
+                    if (this.objects.tickX >= (this.objects.boxX + this.objects.boxWidth) - this.objects.tickWidth) {this.objects.tickX = (this.objects.boxX + this.objects.boxWidth) - this.objects.tickWidth;}
+                    if (this.objects.tickX <this.objects.boxX + this.objects.tickWidth) {this.objects.tickX = this.objects.boxX + this.objects.tickWidth;}
+
+                    if (keys[keybinds.Action]) {this.objects.tickX += this.objects.reverse ? -this.objects.tickSpeed : this.objects.tickSpeed;}
+
+                    if (keyReleases[keybinds.Action]) {//this should be when the dmg is calculated, and we give up the turn
+                        if (this.objects.tickX >= this.objects.padAreaX && this.objects.tickX <= this.objects.padAreaX + this.objects.padAreaWidth) {
+                            battlefield.damage(target,self.attack.current);
+                        }
+                        tweenService.create(self.worldData,tweenService.TweenInfo(0.5,"SineOut"),{x:(self.teamIndex * heightAddon) + allyXBuffer}).play();
+                        this.objects.movingBack = true;
+                        new shakeEffect(target.worldData,25,0.5);
+                        new shakeEffect(target.worldData,250,1);
+                        attackMinigames.existingMinigames.shift();
+                        battlefield.turn++;
+                        setState(battleStates.TURN_START);
+
+                    }
+                };
+                this.draw = function(){
+                    newRect("outlineBoxBGSword",this.objects.boxX - this.objects.boxOutlineSize, this.objects.boxY - this.objects.boxOutlineSize ,this.objects.boxWidth + (this.objects.boxOutlineSize*2),this.objects.boxHeight + (this.objects.boxOutlineSize*2),"rgb(0,0,0)").draw();
+                    newRect("boxBGSword",this.objects.boxX,this.objects.boxY,this.objects.boxWidth,this.objects.boxHeight,this.objects.boxColor).draw();
+                    newRect("padAreaSword",this.objects.padAreaX,this.objects.padAreaY,this.objects.padAreaWidth,this.objects.padAreaHeight,this.objects.padColor).draw();
+                    newRect("tickSword",this.objects.tickX - this.objects.tickWidth,this.objects.tickY,this.objects.tickWidth,this.objects.tickHeight,this.objects.tickColor).draw();
+                    newText("guidance",(gameWidth/2) - 400,this.objects.boxY - 24,"rgb(255,255,255)","24px Arial","Hold and release your ACTION key when the red tick is in the GREEN area!").draw();
+                }
+                break;
+
+
+            case "spell":
+                this.objects = {
+
+                }
+                this.update = function(deltaTime){};
+                this.draw = function(){
+                    newText("guidance2",(gameWidth/2) - 500,800 - 24,"rgb(255,255,255)","24px Arial","Hold your ACTION key to move the tick right, try to keep it in the GREEN area! Let go to move it left, don't let it hit the edges").draw();
+                };
+                break;
+            case "pirate":
+                this.update = function(deltaTime){};
+                break;
+            case "kit" :
+                this.update = function(deltaTime){};
+                break;
+            default:
+                Error("Invalid attack minigame type given");
+                break;
+        }
+        attackMinigames.existingMinigames.push(this);
+    }
+
+}
 
 /*
 *
-* dodgeHandler is where damage is applied, DO NOT ATTEMPT TO DO IT WITHOUT IT
-* timelines are how we know at x time, when we do some visual or auditory
+* dodgeHandler is where damage is applied, DO NOT ATTEMPT TO DO IT WITHOUT IT (for enemy attacks)
+* timelines are how we know at x time, when we do some visual or auditory effect
 *
 * */
