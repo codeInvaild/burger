05a430c46bc320f0265dd1d5874a8c98f0b4ba67
codeInvaild <kongtrong21@gmail.com>
2026-08-08 01:45:03 -0600

    added dodging + 1 basic enemy attack
    Also added extra buffers so you get time to breathe at the end of turns, also Illumine has a bar telling you how completed the spell is

--- Burger Tales-20260517T010439Z-3-001/Burger Tales/actionDirectory.js
+++ Burger Tales-20260517T010439Z-3-001/Burger Tales/actionDirectory.js
@@ -1,9 +1,18 @@
 import { tweenService } from "./TweenService.js";
 import {keys, keybinds, keyPresses, keyReleases} from "./KeyboardInputHandler.js";
-import {allyXBuffer, battle, battlefield, battleStates, heightAddon, heightBuffer, setState} from "./BattleHandler.js";
-import { newText, newRect, randInt,shakeEffect, } from "./Utility.js";
+import {
+    allyXBuffer,
+    battle,
+    battlefield,
+    battleStates,
+    enemyXBuffer,
+    heightAddon,
+    heightBuffer,
+    setState
+} from "./BattleHandler.js";
+import {newText, newRect, randInt, shakeEffect, newRotatedRect,} from "./Utility.js";
 import {gameHeight,gameWidth} from "./main.js";
-import {availableAssets, playSoundEffect} from "./AssetLoader.js";
+import {availableAssets} from "./AssetLoader.js";
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
@@ -61,79 +70,178 @@ export class AnimationTimeline {
 }
 
 let dodgeTypes = {
-    MISS: 1000,
-    OKAY:500,
-    GOOD:350,
+    MISS: 500,
+    OKAY:400,
+    GOOD:300,
     GREAT:200,
     PERFECT:100,
 }
 
-function findDodge(currentTime,endTime) {
-    let time = endTime - currentTime;
+function findDodge(currentTime,endTime,customParameters) {
+    if (dodgeHandler.dodgesActive[0].dodged) {console.log("already dodged!");return null; }
+    let timeDifference = (endTime - currentTime) * 1000;//this is in MILLISECONDS, expect a high number in the thousands/hundreds
 
-    // Sort thresholds from highest to lowest
-    const sorted = Object.entries(dodgeTypes).sort((a, b) => b[1] - a[1]);
-    for (const [label, threshold] of sorted) {
-        if (value >= threshold) return label
-    };
-    // If it's below all thresholds, return the lowest category
-    return Object.keys(sorted[sorted.length - 1][0]).toString();
+    console.log(timeDifference);
+
+    if (timeDifference > 0 && timeDifference <= (customParameters?.PERFECT_TIMING ? customParameters?.PERFECT_TIMING : dodgeTypes.PERFECT)) {
+        return "PERFECT";
+    } else if ( timeDifference > (customParameters?.PERFECT_TIMING ? customParameters?.PERFECT_TIMING : dodgeTypes.PERFECT) && timeDifference <= (customParameters?.GREAT_TIMING ? customParameters?.GREAT_TIMING : dodgeTypes.GREAT)) {
+        return "GREAT";
+    } else if (timeDifference > (customParameters?.GREAT_TIMING ? customParameters?.GREAT_TIMING : dodgeTypes.GREAT) && timeDifference <= (customParameters?.GOOD_TIMING ? customParameters?.GOOD_TIMING : dodgeTypes.GOOD)) {
+        return "GOOD";
+    } else if (timeDifference > (customParameters?.GOOD_TIMING ? customParameters?.GOOD_TIMING : dodgeTypes.GOOD) && timeDifference <= (customParameters?.OKAY_TIMING ? customParameters?.OKAY_TIMING : dodgeTypes.OKAY)) {
+        return "OKAY";
+    } else if (timeDifference > (customParameters?.OKAY_TIMING ? customParameters?.OKAY_TIMING : dodgeTypes.OKAY) && timeDifference <= (customParameters?.MISS_TIMING ? customParameters?.MISS_TIMING : dodgeTypes.MISS)) {
+        return "MISS";
+    } else {
+        console.log("this time didn't fit into any category "+timeDifference);
+        return "NO_REG";
+    }
 }
 
+export let directionTypes = {
+    up:"Up",
+    left:"Left",
+    right:"Right",
+    down:"Down",
+}
+
+
+//do not ever set up a dodge that happens after the animation timeline is completed, the player won't be able to guard against it
 export let dodgeHandler = {
     dodgesActive : [],
     localDelta : 0,
-    debounced : false,
     active : false,
-    target : "",//enemies should never need to dodge, so we always assume it's on the ally side
 
-    queueDodge(time,cost={energy:2,GP:1},damage,modifiers=null) {
-        if (!this.active) {this.active=true; this.localDelta=0;}
-        this.dodgesActive.push({time : time,dodged : false,damage : damage, cost: cost, modifiers : modifiers}) ;
+    queueDodge(time,directions,target,damage,modifiers=null) {
+        // if (!this.active) {this.active=true; this.localDelta=0;}
+        console.log("you just queued a dodge")
+        console.log("your target is: "+target?.name);
+        this.dodgesActive.push({time : time,dodged : false , target: target,damage : damage, directions:directions,directionsPressed:{}, modifiers : modifiers}) ;
+    },
+
+
+
+    //perfect: 100% reduction
+    //great: 75% reduction
+    //okay: 25% reduction
+    //miss: 0% reduction
+    //
+    handleDodge(curDirection,dodgeTime,totalTime,customParams) {
+        let dodge = findDodge(dodgeTime,totalTime,customParams);
+        if (dodge === "NO_REG") {return;}
+        let curDodge = this.dodgesActive[0];
+        let registerHit = true;
+
+        console.log("Handling dodge... "+dodge);
+        switch (dodge) {
+            case "PERFECT":
+                battlefield.damage(curDodge.target, customParams?.PERFECT_DMG_MULT ? customParams?.PERFECT_DMG_MULT : 0);
+                console.log("perfect: 0");
+                break;
+
+                case "GREAT":
+                    battlefield.damage(curDodge.target, customParams?.GREAT_DMG_MULT ? customParams?.GREAT_DMG_MULT : Math.floor(curDodge.damage * 0.25));
+                    console.log("GREAT: "+Math.floor(curDodge.damage * 0.25));
+                    break;
+
+                    case "GOOD":
+                        battlefield.damage(curDodge.target, customParams?.GOOD_DMG_MULT ? customParams?.GOOD_DMG_MULT : Math.floor(curDodge.damage * 0.5));
+                        console.log("GOOD: "+Math.floor(curDodge.damage * 0.5));
+                        break;
+
+                        case "OKAY":
+                            battlefield.damage(curDodge.target, customParams?.OKAY_DMG_MULT ? customParams?.OKAY_DMG_MULT : Math.floor(curDodge.damage * 0.75));
+                            console.log("OKAY: "+Math.floor(curDodge.damage * 0.75));
+                            break;
+
+                            case "MISS":
+                                registerHit = false;
+                                console.log("you missed bruh");
+                                // battlefield.damage(curDodge.target, curDodge.damage);
+                                break;
+        }
+
+        if (registerHit){curDodge.directionsPressed[curDirection] = true;}
+        if (Object.keys(curDodge.directionsPressed).length === Object.keys(curDodge.directions).length) {this.dodgesActive[0].dodged = true;}
     },
 
     update (dt) {
-        this.localDelta += dt*1000;
+        if (this.dodgesActive.length === 0) {return;}
+        console.log(dt);
+        this.localDelta += dt;
 
-        if (keys[keybinds.Action] && !this.debounced) {//pick up on input
-            this.debounced = true;
-            setTimeout(()=> {this.debounced=false},50)
-
-            let dodge = findDodge(this.localDelta,this.dodgesActive[0].time);//we now have what type of dodge it is
-
-            //take away energy and add up GP
-            battlefield.allies[this.target].take("energy",this.dodgesActive[0].cost?.energy);
-
-            //handle the dodge here!
-
-            battlefield.allies[this.target].take("health",
-                dodge === "PERFECT" ? 0 :
-                    dodge === "GREAT" ? this.dodgesActive[0].damage*0.25 :
-                        dodge === "GOOD" ? this.dodgesActive[0].damage*0.5 :
-                            dodge === "OKAY" ? this.dodgesActive[0].damage*0.75 :
-                                dodge === "MISS" ? this.dodgesActive[0].damage : 0
-
-            );
-
-            this.dodgesActive[0].dodged = true;
+        //each needs to check for if it was the correct input or part of it (multi-hits), just compare with this.dodgesActive[0]'s direction array
+        if (keyPresses[keybinds.Up]) {
+            let found = this.dodgesActive[0]?.directions?.Up;
+            if (found) {
+                this.handleDodge("Up",this.localDelta,this.dodgesActive[0].time,this.dodgesActive[0]?.modifiers);
+            } else {
+                availableAssets.sounds["OO_Miss"].play();
+            }
         }
+
+        if (keyPresses[keybinds.Down]) {
+            let found = this.dodgesActive[0]?.directions?.Down;
+            if (found) {
+                this.handleDodge("Down",this.localDelta,this.dodgesActive[0].time,this.dodgesActive[0]?.modifiers);
+            } else {
+                availableAssets.sounds["OO_Miss"].play();
+            }
+        }
+
+        if (keyPresses[keybinds.Left]) {
+            let found = this.dodgesActive[0]?.directions?.Left;
+            if (found) {
+                this.handleDodge("Left",this.localDelta,this.dodgesActive[0].time,this.dodgesActive[0]?.modifiers);
+            } else {
+                availableAssets.sounds["OO_Miss"].play();
+            }
+        }
+
+        if (keyPresses[keybinds.Right]) {
+            let found = this.dodgesActive[0]?.directions?.Right;
+            if (found) {
+                this.handleDodge("Right",this.localDelta,this.dodgesActive[0].time,this.dodgesActive[0]?.modifiers);
+            } else {
+                availableAssets.sounds["OO_Miss"].play();
+            }
+        }
+
 
         if (this.localDelta >= this.dodgesActive[0].time) {
+            if (!this.dodgesActive[0].dodged) {//count as a missed dodge
+                battlefield.damage(this.dodgesActive[0].target, this.dodgesActive[0].damage);
+            }
             let removedElement = this.dodgesActive.shift();
-            this.localDelta -= removedElement.time;
-        }
-        if (this.dodgesActive.length === 0) {
-            this.active = false;
+            this.localDelta = 0;
         }
     },
 
     draw(){
         newRect("dodgeRect",400,800,500,300,"rgb(255,255,255)").draw();
-        newText("dodgeText", 400,800,"rgb(0,0,0)","40px Courier New","press "+keybinds.Action+" to dodge! Be wary of your GP and energy however!").draw();
-
-
-
+        newText("dodgeText", 400,800,"rgb(0,0,0)","40px Courier New","press a corresponding MOVEMENT KEY to dodge at the right time!").draw();
     },
+}
+
+export class enemyActions {//mostly use this for any special edge cases, but a lot of the times for extra props if u wanna use this for multiple enemies but different visual effects
+    static existingEA = [];
+
+    constructor(name, modifiers, self, target) {
+        this.name = name;
+        this.modifiers = modifiers;
+        this.self = self;
+        this.target = target;
+
+        switch (name) {
+            case "":
+                this.update = {
+
+                };
+
+                break;
+        }
+    }
 }
 
 //ACTIONS LOOK FOR A BATTLEFIELD INSTANCE, NOT A DIRECTORY REFERENCE
@@ -141,17 +249,35 @@ export let dodgeHandler = {
 export let actionDirectory = {//THESE ARE USED FOR DODGING
     fight : {
         cost : 1,
-        execute : function(target,targetSide){
+        execute : function(self,target,targetSide){
+            availableAssets.sounds.getReady.play();
             dodgeHandler.target = target;
-            dodgeHandler.queueDodge(1, {energy:0,GP:1},6);
+            //the two lines of code below are an example of the BS you have to do, after one dodge, SUBTRACT from whatever time that was to get your next designated time
+            dodgeHandler.queueDodge(2, {Right:directionTypes.right},target,self.attack.current);
+            dodgeHandler.queueDodge(3, {Right:directionTypes.right},target,self.attack.current);//I want it to happen at timeline.at = 5, so 5-2 = 3
             let timeline = AnimationManager.create(6);
-            timeline.at(0.5,() => {
-                tweenService.create(battlefield[targetSide][target].worldData, tweenService.TweenInfo(1,"SineOut"),{x:400}).play();
+            timeline.at(1,() => {
+                tweenService.create(self.worldData, tweenService.TweenInfo(1,"SineOut"),{x:target.worldData.x + 75,y:target.worldData.y}).play();
             });
+
+            timeline.at(3,()=>{
+                tweenService.create(self.worldData, tweenService.TweenInfo(0.5,"SineOut"),{x:target.worldData.x + 550}).play();
+            })
+
+            timeline.at(4.5,()=>{
+                tweenService.create(self.worldData, tweenService.TweenInfo(0.5,"Linear"),{x:target.worldData.x + 75}).play();
+            })
+
+            timeline.at(6,()=>{
+                tweenService.create(self.worldData, tweenService.TweenInfo(0.5,"SineOut"),{x:enemyXBuffer,y: heightBuffer + (self.teamIndex * heightAddon) }).play();
+                setTimeout(nextTurn(),1500);
+            })
         },
         targetType : "enemies",
     }
 }
+
+
 
 export let playerActionDirectory = {
     slash : {
@@ -163,14 +289,6 @@ export let playerActionDirectory = {
             setTimeout(function(){
                 let c = new attackMinigames("sword",{},self,target);
             },1000)
-                //tween ally toward the enemy (stop right in from of them)
-                //create a sword png, have the player holster it on their back, and swing in an arc
-                //create a hold and release mechanism and determine pt bonus based on success (closer to the center of the green hit zone, the >40 pts you get)
-
-                //create two bars; a brown and a green zone, the green zone is contained within the brown zone and size is determined by a percent modifier
-                //create a red tick that sticks out on the top and bottom that the player can hold their interaction button to hold and reelase from
-
-                //the timeline system sadly can't really work here and I also need some way for this to get input whilst animate
         },
         targetType : "enemies",
     },
@@ -201,6 +319,15 @@ export let playerActionDirectory = {
     bow : {
 
     }
+}
+
+function nextTurn() {
+    attackMinigames.existingMinigames.shift();
+    enemyActions.existingEA.shift();
+    setTimeout(function(){
+        battlefield.turn++;
+        setState(battleStates.TURN_START);
+    },1000);
 }
 
 export class attackMinigames {
@@ -251,23 +378,15 @@ export class attackMinigames {
                             battlefield.damage(target,self.attack.current);
                         } else {
                             battlefield.damage(target,1);
-                            for (let fun = 0; fun < 25;fun++) {
-                                // playSoundEffect("click");
-                                availableAssets.sounds["click"].play();
-                                setTimeout(() => {battlefield.damage(target,fun);},100*fun);
-                            }
-
-
-
-
+                            // for (let fun = 0; fun < 75;fun++) {
+                            //     setTimeout(() => {battlefield.damage(target,fun);availableAssets.sounds["gp"].play({ playbackRate: 0.1 + Math.random() * 3 });},100*fun);
+                            // }
                         }
                         tweenService.create(self.worldData,tweenService.TweenInfo(0.5,"SineOut"),{x:allyXBuffer,y:(self.teamIndex * heightAddon) + heightBuffer}).play();
                         this.objects.movingBack = true;
-
+                        self.execute = null;
                         new shakeEffect(target.worldData,25,0.5);
-                        attackMinigames.existingMinigames.shift();
-                        battlefield.turn++;
-                        setState(battleStates.TURN_START);
+                        nextTurn();
 
                     }
                 };
@@ -283,12 +402,71 @@ export class attackMinigames {
 
             case "spell":
                 this.objects = {
+                    tickWidth : 10,
+                    tickHeight:100,
+                    tickX : 820,
+                    tickY : 800,
+                    tickColor : "rgb(255,0,0)",
+                    tickSpeed: modifiers?.tickSpeed ?? 20,
+                    tickDecay:modifiers?.tickDecay ?? 30,
+
+                    boxWidth : 600,
+                    boxHeight : 100,
+                    boxX: (gameWidth/2) - 300,
+                    boxY : 800,
+                    boxColor : "rgb(37,77,125)",
+
+                    completionWidth : 300,
+                    completionHeight : 100,
+                    completionX: (gameWidth/2) - 150,
+                    completionY : 600,
+                    completionColor : "rgb(47,239,101)",
+
+                    boxOutlineSize : 5,
+
+                    padAreaWidth: modifiers?.padAreaWidth ?? 150,
+                    padAreaHeight:100,
+                    padAreaX: modifiers?.padAreaX ?? randInt(800 + (modifiers?.padAreaWidth ?? 40),400 + 800 - (modifiers?.padAreaWidth ?? 40)),
+                    padAreaY : 800,
+                    padColor : "rgb(62,143,57,0.8)",
+
+                    totalDuration:4,
+                    fillTime:1,
+                    currentFill:0,
 
                 }
                 this.update = function(deltaTime){
+                    let object = this.objects;
+                    if (object.tickX >= object.padAreaX && object.tickX <= object.padAreaX + object.padAreaWidth) {
+                        object.currentFill+=deltaTime;
+                    }
+
+                    if (object.currentFill >= object.fillTime) {//grant the highest bonus
+                        battlefield.damage(target,100);
+                        new shakeEffect(target.worldData,25,0.5);
+                        self.execute = null;
+                        nextTurn()
+                    }
+
+                    if (keys[keybinds.Action]) {
+                        object.tickX+=object.tickSpeed;
+                    } else {
+                        object.tickX-= object.tickDecay;
+                    }
+
+                    if (object.tickX >= object.boxX+object.boxWidth) {
+                        object.tickX=object.boxX+object.boxWidth;
+                    }
+                    if (object.tickX <= object.boxX) {
+                        object.tickX=object.boxX;
+                    }
 
                 };
                 this.draw = function(){
+                    newRect("boxBGStaff",this.objects.boxX,this.objects.boxY,this.objects.boxWidth,this.objects.boxHeight,this.objects.boxColor).draw();
+                    newRect("padAreaStaff",this.objects.padAreaX,this.objects.padAreaY,this.objects.padAreaWidth,this.objects.padAreaHeight,this.objects.padColor).draw();
+                    newRect("tickStaff",this.objects.tickX - this.objects.tickWidth,this.objects.tickY,this.objects.tickWidth,this.objects.tickHeight,this.objects.tickColor).draw();
+                    newRect("staffCompletion",this.objects.completionX,this.objects.completionY,this.objects.completionWidth * (this.objects.currentFill/this.objects.fillTime),this.objects.completionHeight,this.objects.completionColor).draw();
                     newText("guidance2",(gameWidth/2) - 650,800 - 24,"rgb(255,255,255)","24px Arial","Hold your ACTION key to move the tick right, try to keep it in the GREEN area! Let go to move it left, don't let it hit the edges").draw();
                 };
                 break;
