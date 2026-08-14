182413700c90660797ff7773fba24f027afc6c28
codeInvaild <kongtrong21@gmail.com>
2026-08-13 23:01:52 -0600

    Update ItemDirectory.js

--- Burger Tales-20260517T010439Z-3-001/Burger Tales/ItemDirectory.js
+++ Burger Tales-20260517T010439Z-3-001/Burger Tales/ItemDirectory.js
@@ -1,5 +1,102 @@
 export const itemDirectory = {//USABLE ITEMS IN A BATTLE
+    "sugar_apple" : {
+        type:"heal",
+        targetType:"allies",
+        heal:190,
+        description:"A sweet exotic fruit! Heals for 190 hp",
+        image:"sugar_apple",
+        sound:"NFL_Intro",
+    },
+    "cornball" : {
+        type:"heal",
+        targetType:"allies",
+        heal:18,
+        description:"You are so corny for eating this. Heals for 18 hp",
+        image:"cornball",
+        sound:"mrbeast_cringe"
+    },
+    "ketchup" : {
+        type:"heal",
+        targetType:"allies",
+        heal:10,
+        description:"Eating this by itself should be considered criminal. Heals for 10 hp",
+        image:"ketchup",
+        sound:"nutella",
+    }
 
+
+    // "Rotisserie Chicken" : {
+    //     type:"heal",
+    //     heal:65,
+    //     description:"It used to be alive bro",
+    //     image:"rotisserie",
+    // },
+    // "taco" : {
+    //     type:"heal",
+    //     heal:20,
+    //     description:"what an oily taco!",
+    //     image:"taco",
+    // },
+    // "vegetable oil" : {
+    //     type:"heal",
+    //     heal:10,
+    //     description:"why would you drink oil?",
+    //     image:"vegetable_oil",
+    // },
+    // "bomb" : {
+    //     type:"damage",
+    //     damage:5,
+    //     description:"please don't eat this... Use it to explode a radius of enemies instead!",
+    //     image:"bomb",
+    // },
+    // "dumplings" : {
+    //     type:"heal",
+    //     heal:30,
+    //     description:"yummy yummy yummy!",
+    //     image:"dumplings",
+    // },
+    // "Toyota Camry 2009" : {
+    //     type:"damage",
+    //     damage:25,
+    //     description:"Run over an enemy with this! Deals 25 damage on impact",
+    //     image:"toyota",
+    // },
+    // "flounder" : {
+    //     type:"heal",
+    //     heal:5,
+    //     description:"You're disappointed you got this from fishing.",
+    //     image:"flounder",
+    // },
+    // "hot sauce" : {
+    //     type:"debuff",
+    //     debuff:"fire-5",
+    //     description:"so hot it'll burn your enemies. Just try not to eat it yourself...",
+    //     image:"hot_sauce",
+    // },
+    // "REAL sushi" : {
+    //     type:"heal",
+    //     heal:5,
+    //     description:"Average girl talk.",
+    //     image:"real",
+    // },
+    // "FAKE sushi roll" : {
+    //     type: "heal",
+    //     heal: 5,
+    //     description: "MEN",
+    //     image: "freak",
+    // },
+    // "root beer" : {
+    //     type:"heal",
+    //     heal:5,
+    //     description:"🫚🍺",
+    //     image:"root_beer",
+    // },
+    // "Cantaloupe" : {
+    //     type:"heal",
+    //     heal:5,
+    //     description:"Watermelon but as a rock",
+    //     image:"cantaloupe",
+    // },
 }
 
 export const inventoryDirectory = {//MISCELLANEOUS ITEMS
